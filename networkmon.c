9e87e5f09e9f3d1389e0ec09be6e36a22625e1f7
Divewing <Insansf@gmail.com>
2015-05-01 17:15:07 +0700

    add arguments functionality

--- networkmon.c
+++ networkmon.c
@@ -5,8 +5,18 @@
 #include <ncurses.h>
 #include <time.h>
 
-char* cmd_system(const char* command)
+char* cmd_system(char intfc[100],int tipe)
 {
+    char command[100];
+    
+    strcpy(command,"cat /sys/class/net/");
+    strcat(command,intfc);
+    if(tipe==0){
+        strcat(command,"/statistics/rx_bytes");
+    }else if(tipe==1){
+        strcat(command,"/statistics/tx_bytes");
+    }
+    
     char* result = "";
     FILE *fpRead;
     fpRead = popen(command, "r");
@@ -21,11 +31,13 @@ char* cmd_system(const char* command)
     return result;
 }
 
+
 void writetofile(char *det){
     time_t now;
     time(&now);
     strcat(det," #d : ");
     strcat(det,ctime(&now));
+    mvprintw(4,0,"%s",ctime(&now));
     FILE *datbase;
     datbase = fopen("data","w");
 
@@ -34,54 +46,80 @@ void writetofile(char *det){
     fclose(datbase);
 
 }
-int main(int argc, char* argv){
+int main(int argc, char *argv[]){
 initscr(); //Creates stdscr
 mvprintw(0,0,"Downloaded :");
 mvprintw(1,0,"Uploaded   :");
-mvprintw(2,0,"Download :");
-mvprintw(3,0,"Upload   :");
+mvprintw(2,0,"Download   :");
+mvprintw(3,0,"Upload     :");
 mvprintw(2,20,"KB/s");
 mvprintw(3,20,"KB/s");
-char command[100];
+char intfc[100];
 char detail[100];
 char cv[100];
 
 int hasil;
+int delay;
+int i=1;
+
 float kb;
 float kbdlama=0.0;
 float kbulama=0.0;
 float speed;
+
+
+if(argc!=1){
+    for(i;i<argc;i++){
+        if(strcmp(argv[i],"-i")==0){
+            i=i+1;
+            strcpy(intfc,argv[i]);
+        }else if(strcmp(argv[i],"-d")==0){
+            i=i+1;
+            delay=atoi(argv[i]);
+        }
+    }
+}else{
+    strcpy(intfc,"wlan0");
+    delay=1;
+};
+
+//for input check
+//mvprintw(5,0,"%s %s %s %s %s",argv[0],argv[1],argv[2],argv[3],argv[4]);
+//mvprintw(6,0,"%s %s %s %s %i",argv[0],argv[1],intfc,argv[3],delay);
+//delay=1;
+//strcpy(intfc,"wlan0");
 for(;;){
+    strcpy(detail,"#if : ");
+    strcat(detail,intfc);
+    
     //Totaldownloaded
-    strcpy(command,"cat /sys/class/net/wlan0/statistics/rx_bytes");    
-    hasil = atoi(cmd_system(command));
+    hasil = atoi(cmd_system(intfc,0));
     kb = hasil/1000.0;
     mvprintw(0,13,"%i",hasil);
     //Download speed
     if(kbdlama==0.0){
-        mvprintw(2,11,"0.0");
+        mvprintw(2,13,"0.0");
     }else{
         speed = kb-kbdlama;
-        mvprintw(2,11,"%.1f",speed);
+        mvprintw(2,13,"%.1f",speed);
     }
     kbdlama=kb;
     
     sprintf(cv,"%d",hasil);
-    strcpy(detail,"#dt : ");
+    strcat(detail," #dt : ");
     strcat(detail,cv);
     
     //Totaluploaded
-    strcpy(command,"cat /sys/class/net/wlan0/statistics/tx_bytes");
-    hasil = atoi(cmd_system(command));
+    hasil = atoi(cmd_system(intfc,1));
     kb = hasil/1000.00;
     mvprintw(1,13,"%i",hasil);
 
     //Upload speed
     if(kbulama==0.0){
-        mvprintw(3,11,"0.0");
+        mvprintw(3,13,"0.0");
     }else{
         speed = kb-kbulama;
-        mvprintw(3,11,"%.1f",speed);
+        mvprintw(3,13,"%.1f",speed);
     }
     
     kbulama=kb;
@@ -92,7 +130,7 @@ for(;;){
     
     refresh();//Refresh the number
     writetofile(detail);
-    sleep(1);//Delay 1 second
+    sleep(delay);//Delay 1 second
 }
 endwin();
 return 0;
